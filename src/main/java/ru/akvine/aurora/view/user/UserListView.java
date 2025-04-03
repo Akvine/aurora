2f742864893ce458d7937caabf2c8325d13c08c6
akvine <redgooseman55@gmail.com>
2025-04-03 18:50:43 +0300

    init project

---
+++ src/main/java/ru/akvine/aurora/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package ru.akvine.aurora.view.user;
+
+import ru.akvine.aurora.entity.User;
+import ru.akvine.aurora.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
